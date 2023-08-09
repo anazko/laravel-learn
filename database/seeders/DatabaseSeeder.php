fa7cb12445dc972b970c39ec909fc9e0e7b59df0
Andrey Nazarenko <a-naz@mail.ru>
2023-08-09 20:09:03 +0600

    Переделал прикрепление тегов к постам

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,6 @@ namespace Database\Seeders;
 use App\Models\Post;
 use App\Models\Tag;
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,62 +15,43 @@ class DatabaseSeeder extends Seeder
   public function run(): void
   {
 
+    // Create my account with posts
     User::factory()->create([
-        'name' => 'Andrey',
-        'email' => 'andrey@mail.ru',
-        'password' => '123'
-    ])->each(function($user) {
-        Post::factory()
-          ->count(10)
-          ->create(['user_id' => $user->id]);
+      'name' => 'Andrey',
+      'email' => 'andrey@mail.ru',
+      'password' => '123'
+    ])->each(function ($user) {
+      Post::factory()
+        ->count(10)
+        ->create(['user_id' => $user->id]);
     });
 
-    // User::factory()
-    //   ->count(20)
-    //   ->create()
-    //   ->each(function($user) {
-    //     Post::factory()
-    //       ->count(rand(3,10))
-    //       ->create(['user_id' => $user->id])
-    //       ->each(function(Post $post) {
-    //         $post->tags()->sync([1,2,3]);
-    //       });
-    // });
+    // Create tags
+    $tagsList = [
+      'PHP',
+      'Laravel',
+      'Databases',
+      'Docker',
+      'Blade templates',
+      'Javascript'
+    ];
 
-    // Create Tags
-
-    $tagsList = ['PHP', 'Laravel', 'Databases', 'Docker', 'Blade templates', 'Javascript'];
-
-    foreach($tagsList as $tag) {
+    foreach ($tagsList as $tag) {
       Tag::create(['name' => $tag]);
     }
-    
 
-    // Create Users and Posts with relations
+    // Create Users with Posts
     User::factory()
       ->has(Post::factory()->count(10))
       ->count(20)
       ->create();
 
-    // Attach Tags to Posts
-    $tagIds = Tag::all()->map(function ($t) {
-      return $t->id;
-    })->toArray();
-
-    Post::all()->each(function (Post $post) use ($tagIds) {
-      $tagsCount = rand(0, 3);
-      if (!$tagsCount) return;
-      $randomKeys = array_rand($tagIds, $tagsCount);
-      $tags = [];
-      if (is_array($randomKeys)) {
-        foreach ($randomKeys as $key) {
-          $tags[] = $tagIds[$key];
-        }
-      } else {
-        $tags[0] = $tagIds[$randomKeys];
-      }
-      $post->tags()->attach($tags);
-      
-    });
+    // Attach tags to posts
+    $posts = Post::all();
+    $tags = Tag::all();
+    foreach ($posts as $post) {
+      $randomTags = $tags->random(rand(0, 3))->pluck('id');
+      $post->tags()->attach($randomTags);
+    }
   }
 }
